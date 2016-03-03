d9e68c9247574999ee457610294419dc71f69d05
pouyakhast <pkhast@yahoo.com>
2016-03-03 14:03:56 +0000

    created users_controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,14 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
    root 'welcome#index'
+   
+   #articles routes
    resources :articles 
+   
+   #users routes
+   get 'signup', to: 'users#new'
+   resources  :users, except: [:new]
+   
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
