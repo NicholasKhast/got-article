1fede46499a9830f7f82f59609239fc8fa2ea22b
pouyakhast <pkhast@yahoo.com>
2016-03-15 07:18:12 +0000

    Added Categories To the View

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,13 @@ require 'rails/test_help'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  #
+  # Note: You'll currently still have to declare fixtures explicitly in integration tests
+  # -- they do not yet inherit this setting
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  def sign_in_as(user, password)
+    post login_path, session: {email: user.email, password: password}
+  end
 end
