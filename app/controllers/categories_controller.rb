da10b133dcab3e6a420dc3df347207d897c98e9c
pouyakhast <pkhast@yahoo.com>
2016-03-21 06:56:14 +0000

    Modified styling of categories index page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,7 +3,7 @@ class CategoriesController < ApplicationController
   before_action :require_admin, except: [:index, :show]
 
   def index
-    @categories = Category.paginate(page: params[:page], per_page: 5)
+    @categories = Category.paginate(page: params[:page], per_page: 9)
   end
 
   def new
