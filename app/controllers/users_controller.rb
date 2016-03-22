ab1f7dd55f4f197f6d98d5ba66186e4200b45f4b
pouyakhast <pkhast@yahoo.com>
2016-03-22 15:03:56 +0000

    Finished Styling of user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,7 +36,7 @@ class UsersController < ApplicationController
   end
   
   def show
-    @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
+    @user_articles = @user.articles.paginate(page: params[:page], per_page: 3)
   end
 
   def destroy
