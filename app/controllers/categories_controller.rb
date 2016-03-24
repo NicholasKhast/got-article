130c285c11cff1b5e6958987844355c0f7c55b2b
pouyakhast <pkhast@yahoo.com>
2016-03-24 09:45:41 +0000

    Modified Styling of category show page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -36,7 +36,7 @@ end
 
   def show
     @category = Category.find(params[:id])
-    @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
+    @category_articles = @category.articles.paginate(page: params[:page], per_page: 3)
   end
 
   private
